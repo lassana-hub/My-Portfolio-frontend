9804e6bbeafee0cf5c6002eae146701aa75b0074
lassana <lassanabaradji17@icloud.com>
2026-07-13 22:52:04 +0200

    End of Experiences section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -64,9 +64,8 @@ export const Experience = () => {
           </p>
         </div>
         {/* Timeline */}
-        {/* Timeline */}
         <div className="relative">
-          <div className="timeline-glow absolute left-0 md:left-1/2 top-0 bottom-0 w-[2px] bg-gradient-to-b from-primary/70 via-primary/30 to-transparent md:-translate-x-1/2 shadow-[0_0_25px_rgba(32,178,166,0.8)]" />
+          <div className="bg- timeline-glow absolute left-0 md:left-1/2 top-0 bottom-0 w-0.5 bg-linear-to-b from-primary/70 via-primary/30 to-transparent md:-translate-x-1/2 shadow-[0_0_25px_rgba(32,178,166,0.8)]" />
 
           {/* Experience Items */}
           <div className="space-y-12">
